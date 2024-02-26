3096345543fc004fd72e49d75ca57270db0ec6f9
unknown <aurelio.chagas>
2024-02-26 17:32:04 -0300

    criação de botão de tema

--- src/pages/auth/reset-password.tsx
+++ src/pages/auth/reset-password.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export function ResetPassword() {
   return (
-    <div className="p-6 space-y-6 flex flex-col justify-center h-full">
+    <div className="relative p-6 space-y-6 flex flex-col justify-center h-full">
+      <div className="absolute top-4 right-4"><ModeToggle /></div>
       <div className="space-y-2">
         <h1 className="text-3xl font-bold text-center">Recuperação de senha</h1>
         <p className="text-sm text-gray-500 text-center">Redefina sua senha de forma simples e segura para recuperar o acesso à sua conta.</p>
