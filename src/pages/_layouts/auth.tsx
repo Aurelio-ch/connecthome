2aa92567268616f865d7c5425f4389b851be7404
unknown <aurelio.chagas>
2024-02-27 12:36:38 -0300

    teste de erro de gerador de imagem

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 export function AuthLayout() {
   return (
     <div className='grid grid-cols-3 h-screen'>
-      <div className="bg-[url('https://source.unsplash.com/random?condominium')] bg-cover -mb-7 md:mb-0 brightness-75 col-span-2" />
+      <div className="bg-cover -mb-7 md:mb-0 brightness-75 col-span-2" />
       <div >
         <Outlet />
       </div>
